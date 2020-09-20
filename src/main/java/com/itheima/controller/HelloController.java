c8eaf8dc8fea5688ee3a14e5f3766428eb2dd1b7
Betty <Betty@et.com>
2020-09-20 22:48:21 +0800

    add somethings

--- src/main/java/com/itheima/controller/HelloController.java
+++ src/main/java/com/itheima/controller/HelloController.java
@@ -1,14 +1,21 @@
 package com.itheima.controller;
 
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.sql.DataSource;
 
 @RestController
 public class HelloController {
 
+    @Autowired
+    private DataSource dataSource;
+
     @GetMapping("hello")
     public String hello(){
+        System.out.println("dataSource"+dataSource);
         return "Hello springboot";
     }
 }
